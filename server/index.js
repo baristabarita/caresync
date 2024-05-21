69704d4b7293a873bac6a5a5b01f9cdf07909b07
BaristaBarita <18020776@usc.edu.ph>
2024-05-21 14:52:30 +0800

    modified server gitgnore, set up environment variables and index.js file

--- server/index.js
+++ server/index.js
@@ -1,2 +1,27 @@
 require('dotenv').config();
+const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./db/dbConfig.js')
+const cors = require('cors');
+
+const PORT = process.env.PORT || 7000;
+
+const app = require('./routes');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    origin: [process.env.CORS_ORIGIN],
+    methods: ["GET","POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+
+app.get('/', (req,res)=>{
+    res.json({
+        successful: true,
+        data:[1,2,3],
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
